4bb35e656cab5f3a1945ab06a9a7d438cc00a21a
JayakumarS <kannanp@paragondynamics.in>
2022-06-29 11:29:42 +0530

    Test commit

--- src/main/java/com/drug/DrugApp.java
+++ src/main/java/com/drug/DrugApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class DrugApp extends SpringBootServletInitializer {
 	public static void main(String[] args) {
-		System.out.println("Drug App");
+		System.out.println("Drug App"); 
 		SpringApplication.run(DrugApp.class, args);
 	}
 }
