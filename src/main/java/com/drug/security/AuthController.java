3dc057ad0dc9292952a79845d8fcc6d4099d543c
StephenVivek <cvivek7080@gmail.com>
2022-07-07 10:54:45 +0530

    sign in page mail

--- src/main/java/com/drug/security/AuthController.java
+++ src/main/java/com/drug/security/AuthController.java
@@ -1,9 +1,17 @@
 package com.drug.security;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +29,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.drug.common.Email;
 import com.drug.employeeMaster.EmployeeMasterBean;
 import com.drug.employeeMaster.EmployeeMasterService;
 import com.drug.setup.roles.RolesMasterBean;
@@ -69,11 +78,19 @@ public class AuthController {
 		List<RolesMasterBean>  roles = rolesMasterService.getLoginRoleList(userDetails.getUsername());
 		Integer defaultRoleId =  roles.get(0).getRoleId();
 		String defaultRole = roles.get(0).getRoleName();
+		String mailIdReq = "cvivek7080@gmail.com";
+		try {
+			requestMail(mailIdReq);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 		return ResponseEntity.ok(
 				new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles,true,"Sucess",defaultRoleId,defaultRole));
 	}
 
-	@ApiOperation(value = "Get user info by token")
+	@ApiOperation(value = "Get user info by token") 
 	@GetMapping("/userbytoken")
 	public Optional<User> getUserDetail(@RequestParam("token") String jwtToken) {
 		Optional<User> userDetails = null;
@@ -135,6 +152,148 @@ public class AuthController {
 //		
 		
 		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
+	
+	//Mail
+	
+	private String requestMail(String mailId)
+			throws Exception {
+		Email email = new Email();
+		StringBuffer sb = new StringBuffer();
+		String path = "";
+		email.setFromEmailAddress("support@talentchek.com");
+		//mailId = "kathiravan@paragondynamics.in";
+		String toMailAddress = mailId;
+		String[] toEmailIds = toMailAddress.split(",");
+		email.setToEmailAddress(toEmailIds);
+		String logoImage = "https://visitorchek.com/assets/images/vc_logo_1.png";
+		//String logoIdmage = "http://183.82.246.243/assets/images/vc_logo_1.png";
+		//sb.append("<img style='width:86px;height:50px;' src="+logoImage+">");
+
+ 		sb.append("<!DOCTYPE html>\r\n");
+		sb.append(
+				"<html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\">");
+
+		sb.append("<head>\r\n");
+		sb.append("<meta charset=\"UTF-8\">");
+
+		sb.append("<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">");
+		sb.append("<meta name=\"x-apple-disable-message-reformatting\">");
+
+		sb.append("<style>");
+		sb.append(" table, td, div, h1, p {font-family: Arial, sans-serif;}\r\n");
+		
+		sb.append("</style>");
+		sb.append("</head>");
+
+		sb.append("<h1 style=\\\"font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;\\\">Your OTP Number is</h1>");
+//		sb.append("<br>");
+		sb.append("<h2 style=\\\\\\\"font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;\\\\\\\">12536</h2>");
+		
+
+		sb.append("</tr>");
+		sb.append("</table>");
+
+		sb.append("</td>");
+		sb.append("</tr>");
+
+		sb.append("</table>");
+		sb.append("</td>");
+
+		sb.append("</tr>");
+		sb.append("</table>");
+
+		sb.append("</body>");
+		sb.append("</html>");
+
+		email.setBodyHtml(sb.toString());
+		email.setSubject("OTP Number");
+		
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					sendMail(email, path);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		}).start();
+
+		return path;
+	}
+	
+	
+	public static void sendMail(Email email, String path) throws Exception {
+
+		String host = "smtp.sendgrid.net";
+		// Create properties for the Session
+		Properties props = System.getProperties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", "25");
+
+		// Get a session
+		Session session = Session.getInstance(props);
+
+		try {
+			email.setFromEmailAddress("support@talentchek.com");			
+            email.setFromEmailAddress(email.getFromEmailAddress());
+			
+			Transport bus = session.getTransport("smtp");
+
+			bus.connect("smtp.sendgrid.net",25,"apikey", "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");
+			Message msg = new MimeMessage(session);
+
+			// Set message attributes
+			msg.setFrom(new InternetAddress(email.getFromEmailAddress()));
+
+			int n = email.getToEmailAddress().length;
+			InternetAddress[] address = new InternetAddress[n];
+			for (int i = 0; i < n; i++) {
+				address[i] = new InternetAddress(email.getToEmailAddress()[i]);
+
+			}
+			msg.setRecipients(Message.RecipientType.TO, address);
+			
+//			  String toAddress = "sgopes@gmail.com"; InternetAddress[] toAddresses = {
+//			  new  InternetAddress(toAddress) };
+//			  msg.setRecipients(Message.RecipientType.BCC,  toAddresses);
+			 
+			
+			if (email.getCcEmailAddress() != null) {
+				int ccCount = email.getCcEmailAddress().length;
+				InternetAddress[] ccAddress = new InternetAddress[ccCount];
+				for (int i = 0; i < ccCount; i++) {
+					ccAddress[i] = new InternetAddress(email.getCcEmailAddress()[i]);
+				}
+				msg.setRecipients(Message.RecipientType.CC, ccAddress);
+				
+				
+			}
+
+			msg.setSubject(email.getSubject());
+			msg.setSentDate(new Date());
+			msg.setContent(email.getBodyHtml(), "text/html");
+			msg.saveChanges();
+			bus.sendMessage(msg, msg.getAllRecipients());
+			bus.close();
+
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+			while (mex.getNextException() != null) {
+				Exception ex = mex.getNextException();
+				ex.printStackTrace();
+				if (!(ex instanceof MessagingException))
+					break;
+				else
+					mex = (MessagingException) ex;
+			}
+			throw mex;
+		} finally {
+			System.out.println("mail core smtp Successfully");
+		}
 	}
 	
 }
