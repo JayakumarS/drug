9d48aeee4277ac9c4392d8f63e795c4f69855662
Riyaz Ahamed K <riyazahamed1444@gmail.com>
2022-07-22 14:51:54 +0530

    Test commit

--- src/main/java/com/drug/DrugApp.java
+++ src/main/java/com/drug/DrugApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class DrugApp extends SpringBootServletInitializer {
 	public static void main(String[] args) { 
 		System.out.println("Drug App"); 
